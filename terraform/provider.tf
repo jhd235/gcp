bd3469b6e1e1668ffb0c7eccdd237f219240040d
--global <--global>
2025-04-09 15:50:44 +0500

    Enhance Terraform workflow with Google Cloud and GitHub provider setup; add GitHub token variable

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -4,11 +4,19 @@ terraform {
       source  = "hashicorp/google"
       version = "~> 6.28.0"
     }
+    github = {
+      source  = "integrations/github"
+      version = "~> 5.0"
+    }
   }
 }
 
 provider "google" {
-  project     = "soy-transducer-455914-i5"
-  region      = "europe-west10"
-  credentials = file("./soy-transducer-455914-i5-eb4acc5d0af2.json")
+  project = "soy-transducer-455914-i5"
+  region  = "europe-west10"
+  # Credentials will be read from GOOGLE_APPLICATION_CREDENTIALS environment variable
+}
+
+provider "github" {
+  # Token will be read from GITHUB_TOKEN environment variable
 } 
