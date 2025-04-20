cc0b0297878ea3e67f1a7b070643be15de56d973
--global <--global>
2025-04-20 23:00:23 +0500

    Add GKE module configuration for Autopilot cluster and application deployment
    - Introduced new Terraform files for GKE setup, including `app.tf` for application deployment and `cluster.tf` for cluster configuration.
    - Configured a Kubernetes deployment and service for a sample application, along with a Google Compute network and subnetwork for the GKE cluster.
    - Added provider configuration in `provider.tf` for Google Cloud with specified project and region settings.
    - This setup streamlines the deployment of a GKE Autopilot cluster and associated application resources.

---
+++ modules/gke/provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "6.30.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "soy-transducer-455914-i5"
+  region  = "europe-west1" # Default region, can be overridden in resource configurations
+} 
